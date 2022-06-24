2b1a2b02bfac7f5f0624c407807b45325428123d
wilmerrenteria1960 <102265683+wilmerrenteria1960@users.noreply.github.com>
2022-06-24 11:52:48 -0500

    update 7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,11 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#
 import sys
 
 if __name__ == "__main__":
-
     for line in sys.stdin:
-        data = line.strip()
-        data = line.split()
-        sys.stdout.write("{},{},{}\n".format(data[0], data[1], data[2]))
+        data = line.split("   ")
+        sys.stdout.write("{},{},{}\n".format(data[0].strip(),data[2].strip().zfill(4),data[1].strip()))
