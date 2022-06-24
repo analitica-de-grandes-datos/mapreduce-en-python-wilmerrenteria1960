66958098b8f16f95876bdb60a2f9d169bb7b0f60
wilmerrenteria1960 <102265683+wilmerrenteria1960@users.noreply.github.com>
2022-06-24 10:59:23 -0500

    update 4

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     for line in sys.stdin:
         data = line.split(",")
         # se escribe la salida del archivo obteniendo la columna 2 separada por ,
-        sys.stdout.write("{},{}\n".format(data[1],data[0].strip()))
+        sys.stdout.write("{},{}\n".format(data[1].strip(),data[0]))
