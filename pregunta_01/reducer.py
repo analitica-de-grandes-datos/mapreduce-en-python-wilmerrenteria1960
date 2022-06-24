adf5dba0fdf83c68247b56ba0c342032daea4ba5
wilmerrenteria1960 <102265683+wilmerrenteria1960@users.noreply.github.com>
2022-06-24 09:06:54 -0500

    Pregunta 2

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -16,21 +16,12 @@ if __name__ == '__main__':
         val = int(val)
 
         if key == curkey:
-            #
-            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
-            # clave.
-            #
-            total += val
+            if val > total:
+                total= val
+            else:
+                val = total
         else:
-            #
-            # Se cambio de clave. Se reinicia el acumulador.
-            #
             if curkey is not None:
-                #
-                # una vez se han reducido todos los elementos
-                # con la misma clave se imprime el resultado en
-                # el flujo de salida
-                #
                 sys.stdout.write("{}\t{}\n".format(curkey, total))
 
             curkey = key
--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+if __name__ == "__main__":
+    #Se lee el archivo linea a linea
+    for line in sys.stdin:
+        data =line.split(',')
+        sys.stdout.write("{}\t1\n".format(data[2]))
