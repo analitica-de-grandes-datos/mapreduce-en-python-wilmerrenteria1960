bf81c34015caa5ea1e28b1987f14e5ca6f174ecc
wilmerrenteria1960 <102265683+wilmerrenteria1960@users.noreply.github.com>
2022-06-24 10:48:20 -0500

    pregunta3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    
+    
+    for line in sys.stdin:
+        data = line.split(",")
+        # se escribe la salida del archivo obteniendo la columna 2 separada por ,
+        sys.stdout.write("{},{}\n".format(data[1],data[0].strip()))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    
+    # Se lee el archivo linea line.
+    for line in sys.stdin:
+        #se divide la linea del archivo por coma (,)
+        lista = line.split(",")
+        # se escribe la salida del archivo obteniendo la columna 2 separada por ,
+        sys.stdout.write("{},{}\n".format(lista[1].strip(),lista[0]))
